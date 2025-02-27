8db6e6f6051c2e85e7120b5bc821525af582b4a4
ChamikaBandara5 <163339105+ChamikaBandara5@users.noreply.github.com>
2025-02-27 19:26:39 +0530

    test

---
+++ app/src/main/java/com/example/yogademoapp/ThirdActivity.java
@@ -0,0 +1,94 @@
+package com.example.yogademoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    String buttonvalue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        buttonvalue = intent.getStringExtra("value");
+
+        int intvalue = (Integer.valueOf(buttonvalue));
+
+        switch (intvalue){
+
+            case 1:
+                setContentView(R.layout.activity_bow);
+                break;
+
+            case 2:
+                setContentView(R.layout.activity_bridge);
+                break;
+
+            case 3:
+                setContentView(R.layout.activity_chair);
+                break;
+
+            case 4:
+                setContentView(R.layout.activity_child);
+                break;
+
+            case 5:
+                setContentView(R.layout.activity_cobbler);
+                break;
+            case 6:
+                setContentView(R.layout.activity_cow);
+                break;
+
+            case 7:
+                setContentView(R.layout.activity_playji);
+                break;
+
+            case 8:
+                setContentView(R.layout.activity_pauseji);
+                break;
+
+            case 9:
+                setContentView(R.layout.activity_plank);
+                break;
+
+            case 10:
+                setContentView(R.layout.activity_crunches);
+                break;
+
+            case 11:
+                setContentView(R.layout.activity_situp);
+                break;
+
+            case 12:
+                setContentView(R.layout.activity_rotation);
+                break;
+
+            case 13:
+                setContentView(R.layout.activity_twisht);
+                break;
+
+            case 14:
+                setContentView(R.layout.activity_windmill);
+                break;
+
+            case 15:
+                setContentView(R.layout.activity_legup);
+                break;
+        }
+    }
+}
